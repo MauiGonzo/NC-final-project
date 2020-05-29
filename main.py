edb3ab2c00c274b4a3aa1455f6509e5aa0eeb776
sweersr <60323938+sweersr@users.noreply.github.com>
2020-05-29 15:13:07 +0200

    Plotfunction added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 SIR as a Cellular Automaton
 ---------------------------
 
-This code is part of a procject for the course Natural Computing.
+This code is part of a project for the course Natural Computing.
 """
 import numpy as np
 import pandas as pd
@@ -11,9 +11,12 @@ from tkinter import *
 from interface import CellViz
 from grid import Grid
 
+import matplotlib.pyplot as plt
+
 class SIRGui:
 
     FRAME_RATE = 1000           # Miliseconds per frame
+    state_counts = []
 
     def __init__(self, rows, cols, mode='auto'):
         """ Constructs a window in which visualisation will take place. """
@@ -43,6 +46,7 @@ class SIRGui:
         """ This method is run once before the simulation starts. """
         self.SIR = Grid(self.cols, self.rows, radius=1)
         self.SIR.infect(25, 25)
+        self.store_state()
         data = pd.DataFrame(self.SIR.get_states())
         self.cvs.start()
         self.cvs.update(data)
@@ -54,6 +58,13 @@ class SIRGui:
         data = pd.DataFrame(self.SIR.get_states())
         self.cvs.update(data)
         # Keep calling for updates
+
+        self.store_state()
+
+        if not self.find_infected():
+            # end simulation if no infected people are found
+            self.auto_exit()
+
         self.window.after(self.FRAME_RATE, self.update)
 
     def manual_update(self, event):
@@ -66,6 +77,39 @@ class SIRGui:
         """ Exits the application. """
         self.window.destroy()
 
+    def auto_exit(self):
+        """ Exits the application without the need of an event and shows a plot of the simulation. """
+        self.window.destroy()
+        self.plot_states()
+
+    def find_infected(self):
+        """ Determines whether any infected people are left"""
+        return bool(self.state_counts[-1][1])
+
+    def store_state(self):
+        """ Records the number of people in all states"""
+        a = np.zeros(3)
+        for i in self.SIR.cell_list:
+            for j in i:
+                if j.compartment == 'S':
+                    a[0] += 1
+                elif j.compartment == 'I':
+                    a[1] += 1
+                else:
+                    a[2] += 1
+        self.state_counts.append(a)
+
+    def plot_states(self):
+        S = [i[0] for i in self.state_counts]
+        I = [i[1] for i in self.state_counts]
+        R = [i[2] for i in self.state_counts]
+        X = list(range(1, len(S) + 1))
+        plt.plot(X, S, label='Susceptible')
+        plt.plot(X, I, label='Infected')
+        plt.plot(X, R, label='Recovered')
+        plt.legend()
+        plt.show()
+
 
 if __name__ == "__main__":
-    SIRGui(51, 51, 'manual')
+    SIRGui(51, 51, 'auto')
